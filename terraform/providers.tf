c83f4db178ff6556c5d82f5c9098e548aefc2950
ky3ow <vova2341591@gmail.com>
2025-09-21 22:54:51 +0000

    make workspace work

---
+++ terraform/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.55"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  profile = "ky3ow-iam"
+}
+
+provider "databricks" {
+  alias = "mws"
+  host   = "https://accounts.cloud.databricks.com"
+  account_id = var.databricks_account_id
+}
+
+# provider "databricks" {
+#   alias = "root"
+#   host   = databricks_mws_workspaces.root.workspace_url
+# }
---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "databricks_account_id" {
+  default = "d141c655-acae-4662-9c74-db4fe75fc926"
+}
+
+variable "tags" {
+  default = {}
+}
+
+variable "cidr_block" {
+  default = "10.4.0.0/16"
+}
+
+variable "region" {
+  default = "eu-west-1"
+}
+
+resource "random_string" "naming" {
+  special = false
+  upper   = false
+  length  = 6
+}
+
+locals {
+  prefix = "demo${random_string.naming.result}"
+}
